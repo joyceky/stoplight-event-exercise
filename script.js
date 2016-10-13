8f711cdc0a989a4939aac77dd1eaa4aae74bcdfe
Kyle Joyce <kyyjoyce@gmail.com>
2016-10-13 15:41:46 -0600

    finished challenge 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
   Write JS to make this stoplight work.
 
@@ -8,3 +10,27 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+var buttonList = document.getElementById('controls');
+
+// Console log when entering a button
+var logTextEnter = function(event) {
+  if(event.target !== event.currentTarget) {
+  console.log('Entered ' + event.target.textContent + ' Button');
+}};
+
+// Console log when leaving a button
+var logTextExit = function(event) {
+  if(event.target !== event.currentTarget) {
+  console.log('Left ' + event.target.textContent + ' Button');
+}};
+
+// Console log on click
+var logTextClick = function(event) {
+  if(event.target !== event.currentTarget) {
+  console.log(event.target.textContent);
+}};
+
+buttonList.addEventListener('mouseover', logTextEnter, false);
+buttonList.addEventListener('mouseout', logTextExit, false);
+buttonList.addEventListener('click', logTextClick, false);
