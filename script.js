68fe1193288f83324db182d9c0d3ce54fba0d291
Kyle Joyce <kyyjoyce@gmail.com>
2016-10-13 22:46:02 -0600

    add variables, etc

--- script.js
+++ script.js
@@ -15,41 +15,67 @@ var buttonList = document.getElementById('controls');
 var stopLight = document.getElementById('stopLight');
 var slowLight = document.getElementById('slowLight');
 var goLight = document.getElementById('goLight');
+var black = "background-color: black";
+var red = "background-color: red";
+var yellow = "background-color: yellow";
+var green = "background-color: green";
 
 
 // Console log when entering a button
-var logTextEnter = function(event) {
+function logTextEnter(event) {
     if (event.target !== event.currentTarget) {
         console.log('Entered ' + event.target.textContent + ' Button');
     }
 };
 
 // Console log when leaving a button
-var logTextExit = function(event) {
+function logTextExit(event) {
     if (event.target !== event.currentTarget) {
         console.log('Left ' + event.target.textContent + ' Button');
     }
 };
 
 // Console log on click
-var logTextClick = function(event) {
+// var logTextClick = function(event) {
+function logTextClick (event){
     if (event.target !== event.currentTarget) {
         console.log(event.target.textContent + " Clicked");
+        slowLight.setAttribute("style", black);
+        stopLight.setAttribute("style", black);
+        goLight.setAttribute("style", black);
         if (event.target.textContent === "Stop") {
-            stopLight.setAttribute("style", "background-color: red");
-            slowLight.setAttribute("style", "background-color: black");
-            goLight.setAttribute("style", "background-color: black");
+            stopLight.setAttribute("style", red);
         } else if (event.target.textContent === "Slow") {
-            slowLight.setAttribute("style", "background-color: yellow");
-            stopLight.setAttribute("style", "background-color: black");
-            goLight.setAttribute("style", "background-color: black");
+            slowLight.setAttribute("style", yellow);
         } else if (event.target.textContent === "Go") {
-            goLight.setAttribute("style", "background-color: green");
-            slowLight.setAttribute("style", "background-color: black");
-            stopLight.setAttribute("style", "background-color: black");
-        } 
+            goLight.setAttribute("style", green);
+        }
     }
 };
+
+function runTrafficLight() {
+// switch the color based on curren color after timeout
+  var someThing = null;
+  someThing.setTimeout = switchTrafficLightColor(getCurrentLight, getNextLight)
+}
+
+function getCurrentLight() {
+  //get the current lit light using whatever and return it
+  var currentTrafficLight = null;
+
+  return currentTrafficLight;
+}
+
+function getNextLight() {
+  //get the next lit light using whatever and return it
+  var nextTrafficLight = null;
+
+  return nextTrafficLight;
+}
+
+function switchTrafficLightColor(currentLight, NextLight) {
+    //switch shit
+}
 
 buttonList.addEventListener('mouseover', logTextEnter, false);
 buttonList.addEventListener('mouseout', logTextExit, false);
